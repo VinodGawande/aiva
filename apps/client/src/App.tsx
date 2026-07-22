2e6f814a233728b980c0ae2a98979b8dd531407b
vinod gawande <vinodgawande4444@gmail.com>
2026-07-22 12:35:24 +0530

    feat(client): initialize React application

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <h1>AIVA</h1>
+      <p>AI Virtual Interview Assistant</p>
+      <p>Project Initialized Successfully 🚀</p>
+    </main>
+  );
+}
+
+export default App;
